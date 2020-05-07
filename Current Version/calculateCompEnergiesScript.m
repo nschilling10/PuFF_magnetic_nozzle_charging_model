dc2b83ebec92dd34b039cb00867b6c271a166e4d
Nathan Schilling <nschilling10@gmail.com>
2020-05-07 09:31:14 -0500

    Completed Nfcc vs. Rfcc vs. plasma final velocity trade study. The code spit out an optimal magnetic nozzle design with a radius of 1.7m and 100 turns (the maximal amount of turns). Hoenstly, these result seem fairly suspect, becuase when i zoomed in on the plot, after about 11 turns on the mangetic nozzle, it looks like there is spurious oscilation. The amplitude of this oscilation seems fairly low in the grad scheme of things - only 0.4 km/s when the  steady-state value  seems around 250.4km/s. I may re-do the study with more turns on the magnetic nozzle. I'm also considering changing the transformer properties to see how those affect the velocity. I may do that as the next trade study

--- Current Version/calculateCompEnergiesScript.m
+++ Current Version/calculateCompEnergiesScript.m
@@ -1,55 +1,43 @@
-function energies=calCompEnergies(modelOutputs, circInps, plasmaInps)
-%% Energy Calculation function
+%% Energy Calculation Script
 % Nathan Schilling
 % 05/05/2020
 % Holds all the necessary ancilary calcualtions needed to calculate various
 % energies in the circuit (Fcc energy, plasma KE, load capacitor PE,
 % resisitve losses, thermal energy using isentropic flow assumption)
-% INPUTS
-% modelOutputs is assumed to come from the charging circuit model - it
-% should have t, I1, I2, Vfcc, Vcap, R_plasma, and V_plasma. These are all
-% vectors with the same length as the number of entries in the time vector.
-% OUTPUTS
-% energies - a structure holding the energies in the various components
-% over the simulation run. This is in Mega-joules
 
 
 % ---------- Energies in the circuit
 MJ = 1e-6;
 % E_fcc =  .5 *mu0 * Nfcc^2  * dfun(R_plasma)/2 .* I1.^2 *MJ;
-energies.E_fcc =  .5 *mu0 * circInps.Nfcc^2  * circInps.Rfcc/2 .* modelOutputs.I1.^2 *MJ;
-energies.E_plasma = .5*plasmaInps.m_propellant*modelOutputs.V_plasma.^2 *MJ;
-energies.E_cap = .5*circInps.C*modelOutputs.Vcap.^2 *MJ;
+E_fcc =  .5 *mu0 * Nfcc^2  * Rfcc/2 .* I1.^2 *MJ;
+E_plasma = .5*m_propellant*V_plasma.^2 *MJ;
+E_cap = .5*C*Vcap.^2 *MJ;
 
-energies.E_R1=ones(length(t),1);
+E_R1=ones(length(t),1);
 for i=1:length(energies.E_R1)
     if i == 1
-        energies.E_R1(i)=0;
+        E_R1(i)=0;
     else
-        energies.E_R1(i)=circInps.R1*trapz(t(1:i),modelOutputs.I1(1:i).^2)*MJ;
+        E_R1(i)=R1*trapz(t(1:i),I1(1:i).^2)*MJ;
     end
 end
 
-energies.E_R2=ones(length(t),1);
+E_R2=ones(length(t),1);
 for i=1:length(E_R2)
     if i == 1
-        energies.E_R2(1)=0;
+        E_R2(1)=0;
     else
-        energies.E_R2(i)=circInps.R2*trapz(t(1:i),modelOutputs.I2(1:i).^2)*MJ;
+        E_R2(i)=R2*trapz(t(1:i),I2(1:i).^2)*MJ;
     end
 end
-energies.E_ohmic_losses=energies.E_R1+energies.E_R2;
+E_ohmic_losses=E_R1+E_R2;
 
-gamma=plasmaInps.g;
-Rgas=plasmaInps.Rgas;
+T_0=T0+0.5*(gamma-1)*(V_plasma(1).^2/(gamma*Rgas));
 
-T_0=T0+0.5*(gamma-1)*(modelOutputs.V_plasma(1).^2/(gamma*Rgas));
-
-Tf=T_0-0.5*(gamma-1)*(modelOutputs.V_plasma.^2/(gamma*Rgas));
+Tf=T_0-0.5*(gamma-1)*(V_plasma.^2/(gamma*Rgas));
 
 energies.E_therm=3*m_propellant*Rgas*Tf*MJ;
 
 energies.E_tot = E_fcc + E_plasma + E_cap + E_R1 + E_R2 + E_therm; %needed for graph display script
 
 energies.E_tot_2 = E_fcc + E_plasma + E_cap; %needed for graph display script
-end
