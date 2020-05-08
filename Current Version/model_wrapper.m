010eee8ff5c2bc25452ee8ea278237751130331d
Nathan Schilling <nschilling10@gmail.com>
2020-05-08 12:36:09 -0500

    Model run with good recharge, and good thruster characteristics. Trying to get halfway descent Isp out of heavy U235 is a pain in the ass. 1keV doesn't really cut it - the best Isp I could get was like 5000 sec, which is basically the same as an ion engine. Usually runs had an Isp in the Hall thruster range (1800-2200 sec) which is crap. I figured out I had to increase the temperature from 1keV to 50keV. I'm not sure Rob can do that implosion. BUT Isp and impulse bit go up. Netron and heatign load go up by an order of magnitude however, pushing hte limits of design. Playing with the model I realized that a smaller nozzle is better for increased velocity, but a bigger nozzle is better for recharge. Also Nfcc, Rfcc, and I1_0 all affect recharge but also the final velocty. I should probably do a 3D trade study to compare all three, but I'm not sure how to graph it. I think a bigger nozzle is better for recharge because the plasma has mroe time to expand - if Nfcc is too high and I1_0 is too high, the inital magnetic field is too high and the plasma doesn't get to fully expand in the nozzle. Conversely, if it's too low, the plasma slams into the nozzle wall. Anyway, the run in this commit is a good one to look at, reference, and check out. It has an Isp of 7000sec, impulse bit of 10 kNs and a yield of about 1 GJ. It dumps about 20MJ/sr and 50MJ/sr neturon and photon energy fluences however, which is bad.

--- Current Version/model_wrapper.m
+++ Current Version/model_wrapper.m
@@ -10,9 +10,21 @@ clear all
 close all
 
 lectureNotes=false; 
-PUFF=true;
+PUFF=false;
 reactorGradeCaps=false;
-
+%% Physical Constants
+g0=9.80665; %[g0]=m/s^2
+Ru=8.31446261815324e3; %[Ru]=J/k-mol-K
+k_b=8.617333262145e-5; %[k_b]=eV/K
+e=1.602176634e-19; %[e]=J/eV
+eps0=8.8541878128e-12; %[eps0]=SI units
+mu_0=4*pi*1e-7; %[mu_0]=SI units
+h=6.62607015e-34; %[h]=J*s
+m_a=1.6605e-27; %[m_a]=kg/u
+m_e=9.10938356e-31; %[m_e]=kg
+%% Unit conversion
+eV = 1/k_b;
+keV = 1000*eV;
 %% inputs
 plasmaInps.m_propellant = .01;
 if lectureNotes %case used in lecture notes, 40 kV charged to caps
@@ -27,7 +39,7 @@ if lectureNotes %case used in lecture notes, 40 kV charged to caps
     
     %plasma inputs
     plasmaInps.MW = 4;
-    Rgas = 8314.5/MW;
+    Rgas = Ru/MW;
     plasmaInps.T0 = .5; %[T0]=keV
     plasmaInps.g = 1.3;
     
@@ -63,7 +75,7 @@ elseif PUFF  %PUFF
     
     %plasma inputs
     plasmaInps.MW = 4;
-    Rgas = 8314.5/plasmaInps.MW;
+    Rgas = Ru/plasmaInps.MW;
     plasmaInps.T0 = 1*.98; %[T0]=keV
     plasmaInps.g = 1.3;
     
@@ -124,7 +136,7 @@ elseif reactorGradeCaps  %reactor grade caps
     circInps.V20 = 0;  %voltage on reactor caps
 else % your stuff goes here
     
-    plasmaInps.m_propellant = .155;
+    plasmaInps.m_propellant = .150;
     %primary circuit (1) inputs
     circInps.L1 = 100e-9;
     circInps.R1 = 1e-5 * 0;
@@ -136,8 +148,8 @@ else % your stuff goes here
     
     %plasma inputs
     plasmaInps.MW = 235;
-    Rgas = 8314.5/plasmaInps.MW;
-    plasmaInps.T0 = 1;
+    Rgas = Ru/plasmaInps.MW;
+    plasmaInps.T0 = 50;
     plasmaInps.g = 1.3;
     
     %transformer inputs
@@ -145,17 +157,17 @@ else % your stuff goes here
     circInps.k=0.9;
     
     circInps.rT = .1;
-    circInps.NT1 = 100;
+    circInps.NT1 = 300;
     circInps.NT2 = 1; %NT1/10;
     circInps.lT1 = circInps.rT*1;
-    circInps.lT2 = circInps.rT*5;
+    circInps.lT2 = circInps.rT*1;
     
     %flux compression generator inputs
-    circInps.Nfcc = 1;
-    circInps.Rfcc = 10;
+    circInps.Nfcc = 20;
+    circInps.Rfcc = 17;
     
     %-----------initial conditions
-    circInps.I10 = 1e6; %Amps, initial current in primary
+    circInps.I10 = 0.77e6; %Amps, initial current in primary
     circInps.I20 = 0;
 %     circInps.V10 = 0;
     circInps.V20 = 0;  %voltage on reactor caps
@@ -163,3 +175,23 @@ end
 %%
 graphDisplay=true;
 chargingModelOutputs=charging_nozzle_model(circInps,plasmaInps,graphDisplay);
+%% Nozzle parameters
+Vf=-chargingModelOutputs.V_plasma(end);
+Isp=Vf/g0
+Impulse_bit=plasmaInps.m_propellant*Vf
+%% Radiation loads
+Rgas = Ru/plasmaInps.MW;
+Yield=3*plasmaInps.m_propellant*Rgas*plasmaInps.T0*keV %[Yield]=J
+E_fiss=207*1e6*e; %[E_fiss]=J/fiss
+num_fiss=Yield/E_fiss
+neut_per_fiss=2.43;
+tot_neut=neut_per_fiss*num_fiss;
+neturon_fluence_per_steradian=tot_neut/(4*pi)
+
+neut_Energy_per_fiss=5*1e6*e;
+tot_neut_Energy=neut_Energy_per_fiss*num_fiss;
+neturon_Energy_fluence_per_steradian=tot_neut_Energy/(4*pi)
+
+photonEnergy_per_fiss=14*1e6*e;
+tot_photon_energy=photonEnergy_per_fiss*num_fiss;
+tot_photon_energy_per_steradian=tot_photon_energy/(4*pi)
