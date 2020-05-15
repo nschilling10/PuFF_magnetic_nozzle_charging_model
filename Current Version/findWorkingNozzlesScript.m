54e1f1d981ad67a09e664ef48dbd4aa097280ff2
nschilling10 <nschilling10@gmail.com>
2020-05-15 09:57:08 -0500

    This is the script I used to graph the 3D space of nozzles that gave an output velocity that is the same as the input velocity. I found something that looks like a pattern. For nozzles that gave a velocity higher than 32.67km/s (the input velocity) the inital magnetic field was, on average around 50 T. The standard deviation was about 100 T. For nozzles that gave a final velocity higher than the input veloicty (the divergent or error nozzles) the inital magnetic field strength was around 500 T with a standard deviation of 1000 T. For nozzles that gave a final velocity to within +/- 0.005km/s of 32.67 (round to two decimal plaes) the intial magnetic field was around 1000T with a standard deviation of 5000T. This means almost that a higher magnetic field is better; and a lower magnetic field results in either a too-slow final velocity or the simulation diverges. I think.

--- Current Version/findWorkingNozzlesScript.m
+++ Current Version/findWorkingNozzlesScript.m
@@ -1,6 +1,8 @@
 target_vel=32.67;
+precision=2;
+target_vel=round(sqrt(plasmaInps.g*(8314.5/plasmaInps.MW)*2*plasmaInps.T0*1e3*11605)*1e-3,precision);
 Vpf_v=Vpf_tensor(:);
-Inds=find(round(Vpf_v*1e-3,2) == target_vel);
+Inds=find(round(Vpf_v*1e-3,precision) > target_vel);
 [i,j,k]=ind2sub([length(Nfcc_vec) length(Rfcc_vec) length(I10_vec)],Inds);
 Nfcc_work=Nfcc_vec(i);
 Rfcc_work=Rfcc_vec(j);
@@ -13,10 +15,11 @@ set(gca,'zscale','log')
 xlabel('$N_{fcc}$ (\#)','interpreter','latex','fontsize',24)
 ylabel('$R_{fcc}$ (m)','interpreter','latex','fontsize',24)
 zlabel('$I_{1_{0}}$ (MA)','interpreter','latex','fontsize',24)
-title(strcat('Nozzle designs that resulted in $V_{plasma_{f}}$ = ',num2str(target_vel),'km/s +/-0.005km/s'),'interpreter','latex','fontsize',26)
+title(strcat('Nozzle designs that resulted in $V_{plasma_{f}}$ = ',num2str(target_vel),'km/s +/-0.0005km/s'),'interpreter','latex','fontsize',26)
 mu0=4*pi*1e-7;
 Bref=mu0 * Nfcc_work.*I10_work./Rfcc_work;
 figure(19)
 plot(Bref)
 mean(Bref)
-mode(Bref)
+[M,I]=mode(Bref)
+std(Bref)
